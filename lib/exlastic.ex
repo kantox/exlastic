24ec3dd73caa5ab6818d769865417f3abe7f6201
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-12-24 17:57:34 +0100

    Better telemetry.

--- lib/exlastic.ex
+++ lib/exlastic.ex
@@ -22,11 +22,67 @@ defmodule Exlastic do
       now = System.monotonic_time(:microsecond)
       {benchmark, payload} = Keyword.pop(unquote(payload), :benchmark, "N/A")
 
+      # [
+      #   current_function: {Process, :info, 1},
+      #   initial_call: {:proc_lib, :init_p, 5},
+      #   status: :running,
+      #   message_queue_len: 0,
+      #   links: [],
+      #   dictionary: [
+      #     iex_evaluator: :ack,
+      #     iex_history: %IEx.History{
+      #       queue: {[
+      #         {1,
+      #           [:iex_evaluator, :iex_history, :iex_server, :"$initial_call",
+      #           :"$ancestors"]}
+      #       ], []},
+      #       size: 1,
+      #       start: 1
+      #     },
+      #     iex_server: #PID<0.82.0>,
+      #     "$initial_call": {IEx.Evaluator, :init, 4},
+      #     "$ancestors": [#PID<0.82.0>]
+      #   ],
+      #   trap_exit: false,
+      #   error_handler: :error_handler,
+      #   priority: :normal,
+      #   group_leader: #PID<0.65.0>,
+      #   total_heap_size: 9358,
+      #   heap_size: 2586,
+      #   stack_size: 45,
+      #   reductions: 23791,
+      #   garbage_collection: [
+      #     max_heap_size: %{error_logger: true, kill: true, size: 0},
+      #     min_bin_vheap_size: 46422,
+      #     min_heap_size: 233,
+      #     fullsweep_after: 65535,
+      #     minor_gcs: 9
+      #   ],
+      #   suspending: []
+      # ]
+      process_info =
+        self()
+        |> Process.info()
+        |> Map.new()
+        |> Map.take([
+          :status,
+          :message_queue_len,
+          :priority,
+          :total_heap_size,
+          :heap_size,
+          :stack_size,
+          :reductions,
+          :garbage_collection
+        ])
+        |> Map.update!(:garbage_collection, &Map.new/1)
+
       :ok =
         :telemetry.execute(
           event,
           %{
             now: now,
+            schedulers: System.schedulers(),
+            process_info: process_info,
             benchmark: benchmark,
             entity: unquote(entity),
             metadata: %{
